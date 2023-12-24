eb67ff8e34ce75c9ff11b3e4c3215da6cbb55ce2
krisGeorgiev93 <119761546+krisGeorgiev93@users.noreply.github.com>
2023-12-24 23:57:43 +0000

    upload app

---
+++ RunGroopApp/Data/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RunGroopApp.Models;
+
+namespace RunGroopApp.Data
+{
+	public class AppDbContext : DbContext
+	{
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Club> Clubs { get; set; }
+
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<Address> Addresses { get; set; }
+
+        public DbSet<Race> Races { get; set; }
+    }
+}
---
+++ RunGroopApp/Data/Enums/RaceCategory.cs
@@ -0,0 +1,12 @@
+﻿namespace RunGroopApp.Data.Enums
+{
+	public enum RaceCategory
+	{
+
+		Marathon,
+		Ultra,
+		HalfMarathon,
+		Tenk,
+		Fivek
+	}
+}
