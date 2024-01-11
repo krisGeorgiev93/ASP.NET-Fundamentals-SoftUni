948988f8f5b1f6f86cea83bc2b5fb7d45bc22cec
krisGeorgiev93 <119761546+krisGeorgiev93@users.noreply.github.com>
2024-01-11 21:10:32 +0000

    upload

---
+++ SoftUniBazarApp/SoftUniBazar/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿namespace SoftUniBazar.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using SoftUniBazar.Models;
+    using System.Diagnostics;
+
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            if (this.User?.Identity?.IsAuthenticated ?? false)
+            {
+                return RedirectToAction("All", "Ad");
+            }
+
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
