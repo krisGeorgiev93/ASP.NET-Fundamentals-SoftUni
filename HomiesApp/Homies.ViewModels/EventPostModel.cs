39464b34881e4534c9c13e49907f0aa8d0e4fc6b
krisGeorgiev93 <119761546+krisGeorgiev93@users.noreply.github.com>
2024-01-12 22:00:40 +0000

    upload

---
+++ HomiesApp/Homies.ViewModels/EventPostModel.cs
@@ -0,0 +1,32 @@
+﻿namespace Homies.ViewModels
+{
+	using System.ComponentModel.DataAnnotations;
+
+	using static Common.EntityValidations.EventValidation;
+
+	public class EventPostModel
+	{
+        public EventPostModel()
+        {
+            this.Types = new HashSet<TypePostModel>();
+        }
+
+        [Required(AllowEmptyStrings = false)]
+		[StringLength(NameMaxLength, MinimumLength = NameMinLength)]
+		public string Name { get; set; } = null!;
+
+		[Required(AllowEmptyStrings = false)]
+		[StringLength(DescriptionMaxLength, MinimumLength = DescriptionMinLength)]
+		public string Description { get; set; } = null!;
+
+		[Required(AllowEmptyStrings = false)]
+		public string Start {  get; set; } = null!;
+
+		[Required(AllowEmptyStrings = false)]
+		public string End { get; set; } = null!;
+
+		public int TypeId { get; set; }
+
+		public ICollection<TypePostModel> Types { get; set; }
+	}
+}
