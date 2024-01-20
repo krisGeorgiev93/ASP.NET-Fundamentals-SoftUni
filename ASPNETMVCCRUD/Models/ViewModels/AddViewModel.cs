de174a24f5d449150d6384bafd7a1abf7a7c4639
Kristiyan Georgiev <krisgeorgiev027@gmail.com>
2024-01-20 11:36:21 +0200

    upload AddViewModel

---
+++ ASPNETMVCCRUD/Models/ViewModels/AddViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace ASPNETMVCCRUD.Models.ViewModels
+{
+    public class AddViewModel
+    {
+
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public int Salary { get; set; }
+
+        public string Department { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+    }
+}
