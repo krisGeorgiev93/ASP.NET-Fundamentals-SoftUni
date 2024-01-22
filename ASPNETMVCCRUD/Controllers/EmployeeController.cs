66f5378e6c3804a426e492d49a7b5f0daa97994b
Kristiyan Georgiev <krisgeorgiev027@gmail.com>
2024-01-22 16:42:10 +0000

    update

--- ASPNETMVCCRUD/Controllers/EmployeeController.cs
+++ ASPNETMVCCRUD/Controllers/EmployeeController.cs
@@ -1,14 +1,42 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ASPNETMVCCRUD.Data;
+using ASPNETMVCCRUD.Models;
+using ASPNETMVCCRUD.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ASPNETMVCCRUD.Controllers
 {
     public class EmployeeController : Controller
     {
+        private readonly DemoDbContext demoDbContext;
+
+        public EmployeeController(DemoDbContext demoDbContext)
+        {
+            this.demoDbContext = demoDbContext;
+        }
 
         [HttpGet]
         public IActionResult Add()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(AddViewModel empl)
+        {
+            var employee = new Employee()
+            {
+                Id = Guid.NewGuid(),
+                Name = empl.Name,
+                Email = empl.Email,
+                Salary = empl.Salary,
+                Department = empl.Department,
+                DateOfBirth = empl.DateOfBirth,
+            };
+
+           await demoDbContext.Employees.AddAsync(employee);
+           await demoDbContext.SaveChangesAsync();
+
+            return RedirectToAction("Add");
         }
     }
 }
