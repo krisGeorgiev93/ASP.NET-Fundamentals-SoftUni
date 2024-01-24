967ceef51f4ee237c03c99093f3f3f4526e63638
Kristiyan Georgiev <krisgeorgiev027@gmail.com>
2024-01-24 23:24:33 +0000

    delete

--- HomiesApp/Homies.Data/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Homies.Data.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
